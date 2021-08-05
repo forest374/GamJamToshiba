d021c50f53e8d67ff3587135b7b2ed1d95556dfc
Mori＿ｙ <guilty.sabakan34@gmail.com>
2021-08-05 11:24:17 +0900

    バックグラウンド生成

---
+++ Assets/Scripts/Map/BackGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.01f;
+    private void FixedUpdate()
+    {
+        if (this.transform.position.x > -80)
+        {
+            transform.Translate(new Vector2(-moveSpeed, 0));
+        }
+        else
+        {
+            Debug.Log("a");
+            Destroy(this);
+        }
+    }
+}
