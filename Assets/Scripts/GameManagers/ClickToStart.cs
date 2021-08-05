4d8f8a18405e7a90956f70022d2c14d7e8760a24
Mori＿ｙ <guilty.sabakan34@gmail.com>
2021-08-05 13:37:46 +0900

    cilckToStart追加

---
+++ Assets/Scripts/GameManagers/ClickToStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToStart : MonoBehaviour
+{
+    [SerializeField] GameObject clickToStart;
+    [SerializeField] PlayerController player;
+    void Start()
+    {
+        clickToStart.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+
+    public void Click()
+    {
+        player.Move = true;
+        clickToStart.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
